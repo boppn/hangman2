95fed5376eca8613d679720e25530edc1dab44e0
boppn <43018136+boppn@users.noreply.github.com>
2018-09-12 19:08:02 -0400

    First commit

---
+++ src/myFirstProject/HelloWorld.java
@@ -0,0 +1,19 @@
+package myFirstProject;
+
+public class HelloWorld {
+
+	// An object is 2 things.
+	// 1. Actions. Methods.
+	// 2. Attributes. Variables.
+	private String name = "Mike";
+	
+	// main method. Starts the program.
+	// Method Signature - What makes this method unique.
+	public static void main(String[] args) {
+		// Prints to the console.
+		System.out.println("Hello World");
+		System.out.println("My first java app!!!");
+		System.out.println("In 1492, Columbus sailed the ocean blue.");
+	}
+
+}
